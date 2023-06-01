1c7f0e3f54cda36764f2f8c2b0c7ea4a591f0d14
Ilia <32134793+IliaBystrov@users.noreply.github.com>
2023-06-01 17:19:15 +0300

    111111

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-export const Result = (props) =>{
+export const Result = () =>{
+    const res = [{id: 0, ti: 'Какое ваше любимое мороженное?', re: 'шоколадное'}, 
+    {id: 1, ti: 'В каком городе вы живете?', re: 'Дубна'}];
+    const Res_title = "Тестовое название опроса";
 
     return(
         <div id="res_col">
-            <h1 id = 'res_tit'>{"Результаты опроса: " + props.title}</h1>
+            <h1 id = 'res_tit'>{"Результаты опроса: " + Res_title}</h1>
             <h1>----------------------------</h1>
             {
-                props.res_answ.map((i) => {
+                res.map((i) => {
                     return(
                         <div id="res_item" key={i.id}>
                             <h1>{i.ti + ': '}</h1>
--- src/components/mainbuttons.js
+++ src/components/mainbuttons.js
@@ -24,16 +24,17 @@ export const MainButtoms = () =>{
     const ReloadCardList =()=>{
         var title = document.getElementById("InSearch");
         setCard((prevState) => []);
-        //Тут надо получить данные из бд и запихнуть их в cards
-        //Get date from bd -> cards [{id: , title: , link: , data: }]
-        const cards = [{id:3, title: "Title", link: "link", data: "02.02.2022"},
-        {id:4, title: "Title2", link: "link", data: "02.02.2023"}]
+
+        const cards = [
+            {id:3, title: "Title", link: "link", data: "02.02.2022"},
+            {id:4, title: "Title2", link: "link", data: "02.02.2023"}
+        ]
 
         cards.map((card) => {
             addcard(card.id, card.title, card.link, card.data);
         })
     };
-    //Работает
+
     const addcard = (c_id, c_title, c_link, c_data) => {
 
         var newcard = {
@@ -45,28 +46,11 @@ export const MainButtoms = () =>{
         setCard((prevState) => [...prevState, newcard]);
     };
 
-        //Копировать шаблон
-        const CopyShablon =(id)=>{
-            var s_copy = Object.assign([], shablon_cards);
-            var i_copy = [];
-            var new_shablon;
-            for(var i = 0; i<s_copy.length; i++){
-                if(s_copy[i].id === id){
-                    new_shablon = {
-                        id: s_copy[i].id,
-                        title: s_copy[i].title,
-                        link: s_copy[i].link,
-                        data: s_copy[i].data,
-                    }
-                    break;
-                }
-            }
-            console.log(new_shablon);
-            //Здесь надо получить записи о вопросах данного шаблона
-            //из бд и запихнуть их в i_copy, после чего записать
-            //данные из new_shablon и i_copy в бд, если нельзя создать копию сразу на беке.
-            ReloadCardList();
-        };
+    //Копировать шаблон
+    const CopyShablon =(id)=>{
+        ReloadCardList();
+    };
+
     //Удалить шаблон //Сюда надо подкрутить удаление шаблона
     const DeleteShablon =(id)=>{
         console.log("delete - " + id);
@@ -75,41 +59,21 @@ export const MainButtoms = () =>{
         var title = document.getElementById("InSearch");
         ReloadCardList(title.value);
     };
+
     //Копировать ссылку на шаблон - работает
     const CopyLink =(link)=>{
         console.log(link);
         navigator.clipboard.writeText(link);
     };
+
     //Редактировать шаблон
     const EditShablon =(id)=>{
 
-        /*setValue((prevState) => []);
-
-        //Получаем структуру из вопросов конкретного шаблона
-        //в виде x = [{id: 1, question_type: '', question_text: '', answer: [] }]
-
-        var x = [];
-        for(var j=0;j<x.length;j++){
-            var newitem = {
-                id: x[j].id,
-                question_type: x[j].question_type,
-                question_text: x[j].question_text,
-                answer: x[j].answer
-            };
-            setValue((prevState) => [...prevState, newitem]);
-        }
-
-        let title = document.getElementById('shablon_title');
-        title.value="Название";
-        let stype = document.getElementById("typeselect");
-        stype.value="public";*/
-        let bl2 = document.getElementById('mainp');
-        bl2.hidden = false;
     }
-        //Показать результаты
-        const ShowResult =(id)=>{
-            
-        }
+
+    //Показать результаты
+    const ShowResult =(id)=>{     
+    }
 
 
         const items = shablon_cards.map((lst) =>
